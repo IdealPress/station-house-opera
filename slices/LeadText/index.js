79ae406b40f0188534d23bcc5497ddc680c47636
Jack Clarke <hello@idealpress.org>
2022-12-12 21:27:00 +0000

    Update index.js

--- slices/LeadText/index.js
+++ slices/LeadText/index.js
@@ -4,8 +4,8 @@ import { PrismicRichText } from "@prismicio/react";
 import styles from "./LeadText.module.css"
 
 const LeadText = ({ slice }) => (
-  <section className="px-6 lg:px-12">
-    <div className={styles.base}>
+  <section className="px-6">
+    <div className="prose prose-lg mx-auto leading-snug">
       {slice.primary.text && <PrismicRichText field={slice.primary.text} />}
     </div>
   </section>
