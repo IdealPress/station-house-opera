aff606586dab9325ffec52ec5041c1605038044f
Jack Clarke <hello@idealpress.org>
2023-01-24 21:40:25 +0000

    Integrate Prismic Preview

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
+import { PrismicProvider } from "@prismicio/react";
+import { PrismicPreview } from "@prismicio/next";
+import { linkResolver, repositoryName } from "../prismicio";
+
 import "styles/globals.css";
 import LightboxProvider from "lib/LightboxProvider";
 
 export default function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
   return (
-    <LightboxProvider>
-      {getLayout(<Component {...pageProps} />)}
-    </LightboxProvider>
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props }) => (
+        <Link href={href}>
+          <a {...props}>{children}</a>
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <LightboxProvider>
+          {getLayout(<Component {...pageProps} />)}
+        </LightboxProvider>
+      </PrismicPreview>
+    </PrismicProvider>
   );
 }
